fc867d0302ec67eebb170e2ef485c6e379ab68c5
55011212070_Champ_xCode <basilisk_champ@hotmail.com>
2014-08-15 16:07:31 +0700

    Finish work
    Add all command Succeed

--- SwiftTutorial1.playground/section-1.swift
+++ SwiftTutorial1.playground/section-1.swift
@@ -3,3 +3,18 @@ let editorialTeam = 23
 var totalTeam = tutorialTeam + editorialTeam
 
 totalTeam+=1
+let priceIntInferred = -19
+let priceIntExpLicit:Int = -19
+
+let priceDoubleInferred = -19.99
+let priceDoubleExpLicit:Double = -19.99
+
+let priceFloatInferred = -19.99
+let priceFloatExpLicit:Float = -19.99
+
+let onSaleInferred = true
+let onSaleExpLicit:Bool = false
+
+let nameInferred = "Whoopie Cushion"
+let nameExpLicit:String = "Whoopie Cushion"
+
