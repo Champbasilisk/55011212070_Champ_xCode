455e74a6dd40b446ce41dd5375cc394fa17e3a14
55011212021prince <iMac@MSU.Prince>
2014-08-22 14:18:27 +0700

    add comment and insert,remove

--- Array_Swift_WMAP.playground/section-1.swift
+++ Array_Swift_WMAP.playground/section-1.swift
@@ -1,25 +1,27 @@
 var shoppingListExplicit = [String]()
-shoppingListExplicit = ["Eggs","Milk"]
-//var shoppingListExplicit: [String] = ["Eggs","Milk"]
+shoppingListExplicit = ["Eggs","Milk"] //นิยมใช้
+//var shoppingListExplicit: [String] = ["Eggs","Milk"] //การประกาศ array
 
 var shoppingList = ["Eggs","Milk"]
 println("The shopping list contains\(shoppingList.count) items.")
 
+//---------การเพิ่มข้อมูลลง array----------//
 shoppingList.append("Flour")
-shoppingList += ["Baking Power"]
+shoppingList += "Baking Power"
 shoppingList += ["Chocolate Spreed","Cheese","Butter"]
 
-var firstItem = shoppingList[0]
+var firstItem = shoppingList[0] //เรียก index
 shoppingList[0] = "Six eggs"
-//shoppingList[0...3] = ["Banana","Apples"]
+shoppingList[0...3] = ["Banana","Apples"] //วน loop 0-3
 
 let possibleTipsInferred = [0.5, 0.18, 0.20]
 let possibleTipExlicit:[Double] = [0.5, 0.18, 0.20]
-var numberOhItem = possibleTipsInferred.count
+var numberOhItem = possibleTipsInferred.count //นับจำนวนใน Array
 
 possibleTipsInferred.isEmpty
 
-shoppingList.insert("pizza", atIndex: 0)
+//-------------เพิ่มค่าใน Array-------------//
+shoppingList.insert("maple syrup", atIndex: 0)
 shoppingList
 
 shoppingList.append("Flour")
