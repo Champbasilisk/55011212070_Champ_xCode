838845c0a47ea8dda2390f698d03e46ede97b807
55011212021prince <iMac@MSU.Prince>
2014-08-22 14:51:35 +0700

    replace method array

---
+++ dictionaryCal.playground/section-1.swift
@@ -0,0 +1,36 @@
+class TipCalculator{
+    
+    //2
+    let total: Double
+    let taxPct: Double
+    let subtotal: Double
+    
+    //3
+    init(total:Double, taxPct:Double){
+        self.total = total
+        self.taxPct = taxPct
+        subtotal = total / (taxPct + 1)
+    }
+    //4
+    func calcTipWithTipPct(tipPct:Double) ->Double{
+        return subtotal * tipPct
+    }
+//-----------------code replace-----------------//
+    //1
+    func returnPossibleTips() -> [Int: Double]{
+        let possibleTipsInferred = [0.15, 0.18, 0.20]
+        let possibleTipExplicit:[Double] = [0.15, 0.18, 0.20]
+        
+        //2
+        var retval = Dictionary<Int, Double>()
+        for possibleTip in possibleTipsInferred{
+            let intPct = Int(possibleTip*100)
+            //3
+            retval[intPct] = calcTipWithTipPct(possibleTip)
+        }
+        return retval
+    }
+}
+
+//
+let tipCalc = TipCalculator(total: 33.25, taxPct: 0.06)
