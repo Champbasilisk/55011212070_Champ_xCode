49f623d751f5682d1a451c29eb3262e7dc2b0043
Thanachoat Jiamwattananukul <basilisk_champ@hotmail.com>
2014-09-03 15:11:06 +0700

    add function

--- usernameSys.playground/section-1.swift
+++ usernameSys.playground/section-1.swift
@@ -19,11 +19,33 @@ class userSys {
             println("Change successful \(oldUser).")
         }
     }
-    
+     /*-- Add count function--*/
     func totalUser() -> Int{
         var total=userID.count
         return total
     }
+     /*-- Add addList function--*/
+    func addList(newUser:Dictionary<String,String>){
+        for (key,values) in newUser{
+            userID[key] = values
+            println("key: \(key) value:\(values)")
+        }
+        for userName in newUser.keys{
+            println("User name : \(userName)")
+        }
+        
+        for password in newUser.values{
+            println("Password : \(password)")
+        }
+    }
+    /*-- Add Remove user function--*/
+    func removeUser(userName:String){
+        if let removeUser = userID.removeValueForKey("\(userName)"){
+            println("Remove user name\(removeUser).")
+        } else {
+            println("User not exist")
+        }
+    }    
 }
 let userSystem = userSys(userName: "Champ", password: "1234")
 userSystem.addID("Test", password: "12345")
@@ -37,3 +59,6 @@ userSystem.userID
 /*------------ update new function 3/9/14-----------*/
 
 userSystem.totalUser()
+userSystem.removeUser("admin")
+userSystem.removeUser("bbb")
+userSystem.addList(["Test1":"0000","Test2":"1111","Test3":"2222"])
