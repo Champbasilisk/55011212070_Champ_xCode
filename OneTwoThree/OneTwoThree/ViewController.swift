f91cda4bbaf7857ac52fa84c9f6bf3347b86903f
Thanachoat Jiamwattananukul <basilisk_champ@hotmail.com>
2014-09-12 16:51:14 +0700

    Lab One Two Tree

---
+++ OneTwoThree/OneTwoThree/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  OneTwoThree
+//
+//  Created by student on 9/12/14.
+//  Copyright (c) 2014 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var LOne: UILabel!
+    @IBOutlet var LTwo: UILabel!
+    @IBOutlet var LThree: UILabel!
+    
+    @IBAction func Clickone(sender: AnyObject) {
+        LOne.text = "1"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
