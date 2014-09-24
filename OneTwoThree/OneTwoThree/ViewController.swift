e4d96e22df128e434c1fc8a06607e355698cad33
Thanachoat Jiamwattananukul <basilisk_champ@hotmail.com>
2014-09-24 14:41:59 +0700

    1-2-3 Count Success

--- OneTwoThree/OneTwoThree/ViewController.swift
+++ OneTwoThree/OneTwoThree/ViewController.swift
@@ -13,9 +13,32 @@ class ViewController: UIViewController {
     @IBOutlet var LOne: UILabel!
     @IBOutlet var LTwo: UILabel!
     @IBOutlet var LThree: UILabel!
-    
+
+    var num1 = 0
+    var num2 = 0
+    var num3 = 0
     @IBAction func Clickone(sender: AnyObject) {
-        var num = 0
+        num1 += 1
+        LOne.text = "\(num1)"
+        
+    }
+    @IBAction func Clicktwo(sender: AnyObject) {
+        num2 += 1
+        LTwo.text = "\(num2)"
+    }
+    @IBAction func Clickthree(sender: AnyObject) {
+        num3 += 1
+        LThree.text = "\(num3)"
+    }
+    
+    @IBAction func ClickReset(sender: AnyObject) {
+        var num1 = 0
+        var num2 = 0
+        var num3 = 0
+        
+        LOne.text = "0"
+        LTwo.text = "0"
+        LThree.text = "0"
     }
     
     override func viewDidLoad() {
