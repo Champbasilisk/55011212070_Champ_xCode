8afba8c38824a286f8aebb4bc1a3a612f1e2da86
Student <student@tests-iMac-18.local>
2014-10-24 17:24:00 +0700

    Up

--- SeguePrac/SeguePrac/ViewController.swift
+++ SeguePrac/SeguePrac/ViewController.swift
@@ -17,7 +17,15 @@ class ViewController: UIViewController,ColorTwoViewControllerDelegate {
     }
     func myVCDidFinish(controller: ColorTwoViewController, text: String) {
         colorlabel.text = text
-
+        if(text=="Red"){
+            colorlabel.backgroundColor = UIColor(red: 250/255.0, green: 154/255.0, blue: 150/255.0, alpha: 1.0)
+        }
+        else if(text=="Green"){
+            colorlabel.backgroundColor = UIColor(red: 157/255.0, green: 254/255.0, blue: 188/255.0, alpha: 1.0)
+        }
+        else if(text=="Blue"){
+            colorlabel.backgroundColor = UIColor(red: 144/255.0, green: 200/255.0, blue: 253/255.0, alpha: 1.0)
+        }
         controller.navigationController?.popViewControllerAnimated(true)
     }
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
